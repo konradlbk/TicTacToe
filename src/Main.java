f0fa7449bc796b771aea5dde5b273daecf4aec9b
Konrad_Szulc <konrad0530@gmail.com>
2018-04-24 18:59:37 +0200

    Added small fixes

--- src/Main.java
+++ src/Main.java
@@ -18,121 +18,124 @@ public class Main {
         }
 
 
-        for (int w = 0; w < board.length; w++) {
+        for (int k = 0; k < board.length; k++) {
+            if (didyouWin(board) == true) {
+                break;
+            }
+            for (int w = 0; w < board.length; w++) {
 
 
-            while (board[w][w].contains(" ") && didyouWin(board) == false) {
+                while (board[k][w].contains(" ") && didyouWin(board) == false) {
 
-                System.out.println("Kolej gracza X");
-                String sign = scanner.nextLine();
-                if (sign.contains("X")){
+                    System.out.println("Kolej gracza X");
+                    String sign = scanner.nextLine();
+                    if (sign.contains("X")) {
 
-                    System.out.println("Podaj lokalizację");
-                } else {
-                    System.out.println("Podano nieprawidlowy znak");
-                    continue;
-                }
-                int lokalizacjaX = scanner.nextInt();
-                int lokalizacjaY = scanner.nextInt();
-                scanner.nextLine();
-                if (board[lokalizacjaX][lokalizacjaY].contains(" ")) {
-                    for (int i = 0; i < board.length; i++) {
-                        for (int j = 0; j < board.length; j++) {
-                            board[lokalizacjaX][lokalizacjaY] = sign;
-                            System.out.print("\t" + board[i][j] + " | ");
+                        System.out.println("Podaj lokalizację");
+                    } else {
+                        System.out.println("Podano nieprawidlowy znak");
+                        continue;
+                    }
+                    int lokalizacjaX = scanner.nextInt();
+                    int lokalizacjaY = scanner.nextInt();
+                    scanner.nextLine();
+                    if (board[lokalizacjaX][lokalizacjaY].contains(" ")) {
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board.length; j++) {
+                                board[lokalizacjaX][lokalizacjaY] = sign;
+                                System.out.print("\t" + board[i][j] + " | ");
 
+                            }
+
+                            System.out.println();
                         }
 
-                        System.out.println();
-                    }
+                    } else {
+                        System.out.println("To pole jest już zajęte");
 
-                } else {
-                    System.out.println("To pole jest już zajęte");
+                        while (board[lokalizacjaX][lokalizacjaY].contains("X") ||
+                                board[lokalizacjaX][lokalizacjaY].contains("O")) {
 
-                    while (board[lokalizacjaX][lokalizacjaY].contains("X") ||
-                            board[lokalizacjaX][lokalizacjaY].contains("O")){
-
-                        System.out.println("Podaj inną lokalizację");
-                        lokalizacjaX = scanner.nextInt();
-                        lokalizacjaY = scanner.nextInt();
-                        scanner.nextLine();
-                    }
-
-                    for (int i = 0; i < board.length; i++) {
-                        for (int j = 0; j < board.length; j++) {
-                            board[lokalizacjaX][lokalizacjaY] = sign;
-                            System.out.print("\t" + board[i][j] + " | ");
-
+                            System.out.println("Podaj inną lokalizację");
+                            lokalizacjaX = scanner.nextInt();
+                            lokalizacjaY = scanner.nextInt();
+                            scanner.nextLine();
                         }
 
-                        System.out.println();
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board.length; j++) {
+                                board[lokalizacjaX][lokalizacjaY] = sign;
+                                System.out.print("\t" + board[i][j] + " | ");
+
+                            }
+
+                            System.out.println();
+                        }
                     }
-                }
 
-                if (didyouWin(board) == true) {
-                    break;
-                }
-                System.out.println("Kolej gracza O");
-                sign = scanner.nextLine();
-                if (sign.contains("O")){
-
-                    System.out.println("Podaj lokalizację");
-                } else {
-                    System.out.println("Podano nieprawidlowy znak");
-                    while (!sign.contains("O")){
-                        System.out.println("Podaj prawidlowy znak");
-                        sign = scanner.nextLine();
-
+                    if (didyouWin(board) == true) {
+                        break;
                     }
-                    System.out.println("Podaj lokalizację");
+                    System.out.println("Kolej gracza O");
+                    sign = scanner.nextLine();
+                    if (sign.contains("O")) {
 
-                }
-                lokalizacjaX = scanner.nextInt();
-                lokalizacjaY = scanner.nextInt();
-                scanner.nextLine();
-                if (board[lokalizacjaX][lokalizacjaY].contains(" ")) {
-                    for (int i = 0; i < board.length; i++) {
-                        for (int j = 0; j < board.length; j++) {
-                            board[lokalizacjaX][lokalizacjaY] = sign;
-                            System.out.print("\t" + board[i][j] + " | ");
+                        System.out.println("Podaj lokalizację");
+                    } else {
+                        System.out.println("Podano nieprawidlowy znak");
+                        while (!sign.contains("O")) {
+                            System.out.println("Podaj prawidlowy znak");
+                            sign = scanner.nextLine();
 
                         }
+                        System.out.println("Podaj lokalizację");
 
-                        System.out.println();
                     }
+                    lokalizacjaX = scanner.nextInt();
+                    lokalizacjaY = scanner.nextInt();
+                    scanner.nextLine();
+                    if (board[lokalizacjaX][lokalizacjaY].contains(" ")) {
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board.length; j++) {
+                                board[lokalizacjaX][lokalizacjaY] = sign;
+                                System.out.print("\t" + board[i][j] + " | ");
 
-                } else {
-                    System.out.println("To pole jest już zajęte");
-                    while (board[lokalizacjaX][lokalizacjaY].contains("X") ||
-                            board[lokalizacjaX][lokalizacjaY].contains("O")){
+                            }
 
-                        System.out.println("Podaj inną lokalizację");
-                        lokalizacjaX = scanner.nextInt();
-                        lokalizacjaY = scanner.nextInt();
-                        scanner.nextLine();
-                    }
-                    for (int i = 0; i < board.length; i++) {
-                        for (int j = 0; j < board.length; j++) {
-                            board[lokalizacjaX][lokalizacjaY] = sign;
-                            System.out.print("\t" + board[i][j] + " | ");
-
+                            System.out.println();
                         }
 
-                        System.out.println();
+                    } else {
+                        System.out.println("To pole jest już zajęte");
+                        while (board[lokalizacjaX][lokalizacjaY].contains("X") ||
+                                board[lokalizacjaX][lokalizacjaY].contains("O")) {
+
+                            System.out.println("Podaj inną lokalizację");
+                            lokalizacjaX = scanner.nextInt();
+                            lokalizacjaY = scanner.nextInt();
+                            scanner.nextLine();
+                        }
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board.length; j++) {
+                                board[lokalizacjaX][lokalizacjaY] = sign;
+                                System.out.print("\t" + board[i][j] + " | ");
+
+                            }
+
+                            System.out.println();
+                        }
+
+                    }
+                    if (didyouWin(board) == true) {
+                        break;
                     }
 
-                }
-                if (didyouWin(board) == true) {
-                    break;
-                }
 
-
+                }
 
             }
-
         }
     }
-
 
 
     public static boolean didyouWin(String[][] check) {
